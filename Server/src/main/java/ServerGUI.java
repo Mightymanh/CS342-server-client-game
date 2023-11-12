d628496d5b22ff83ce3f896e33cadf85dd49bcae
Manh Phan <dphan8@uic.edu>
2023-11-11 23:37:02 -0600

    First commit

---
+++ Server/src/main/java/ServerGUI.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class ServerGUI extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	//feel free to remove the starter code from this method
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("Server GUI");
+
+	}
+
+}
